05573728788f67b89387ea93f7322728453c7a9c
Srikanth Banda <bandasrikanth97@gmail.com>
2024-12-05 19:47:08 -0500

    Updated packages

--- app.py
+++ app.py
@@ -12,9 +12,17 @@ import os
 from pdf2image import convert_from_path
 import pytesseract
 from tempfile import NamedTemporaryFile
+import shutil
 
 # Ensure Tesseract is configured
-pytesseract.pytesseract.tesseract_cmd = r'/opt/homebrew/bin/tesseract'  # Update for your system
+# pytesseract.pytesseract.tesseract_cmd = r'/opt/homebrew/bin/tesseract'  # Update for your system
+
+# Check if Tesseract is installed
+tesseract_path = shutil.which("tesseract")
+if not tesseract_path:
+    raise RuntimeError("Tesseract OCR is not installed or not in PATH.")
+pytesseract.pytesseract.tesseract_cmd = tesseract_path
+
 print(pytesseract.pytesseract.tesseract_cmd)
 def get_pdf_text_with_ocr(uploaded_file):
     """Extract text from a Streamlit UploadedFile using OCR."""
